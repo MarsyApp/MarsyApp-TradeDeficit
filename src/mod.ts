ebd61aaa1b8491777161c19ce0115e6d914cffb1
MarsyApp <>
2023-07-24 15:31:17 +0700

    разлок предметов: скрафченных и принесенных дикими

--- src/mod.ts
+++ src/mod.ts
@@ -28,15 +28,32 @@ import {QuestStatus} from "@spt-aki/models/enums/QuestStatus";
 import {PreAkiModLoader} from "@spt-aki/loaders/PreAkiModLoader";
 import {IAcceptQuestRequestData} from "@spt-aki/models/eft/quests/IAcceptQuestRequestData";
 import {IQuest, Reward} from "@spt-aki/models/eft/common/tables/IQuest";
+import {HideoutCallbacks} from "@spt-aki/callbacks/HideoutCallbacks";
+import {IHideoutTakeProductionRequestData} from "@spt-aki/models/eft/hideout/IHideoutTakeProductionRequestData";
+import {IDatabaseTables} from "@spt-aki/models/spt/server/IDatabaseTables";
+import {DatabaseServer} from "@spt-aki/servers/DatabaseServer";
+import {IHideoutProduction} from "@spt-aki/models/eft/hideout/IHideoutProduction";
+import {InventoryHelper} from "@spt-aki/helpers/InventoryHelper";
+import {IAddItemRequestData} from "@spt-aki/models/eft/inventory/IAddItemRequestData";
+import {ItemChanges} from "@spt-aki/models/eft/itemEvent/IItemEventRouterBase";
 
 class Mod implements IPreAkiLoadMod
 {
     private static container: DependencyContainer;
     private router: DynamicRouterModService;
+    private table: IDatabaseTables;
+    private hideoutProductions: IHideoutProduction[];
     private jsonUtil: JsonUtil;
     private selfSessionId: string;
     private modLoader: PreAkiModLoader;
     private path = require("path");
+
+    public postAkiLoad(container: DependencyContainer): void
+    {
+        this.modLoader = container.resolve<PreAkiModLoader>("PreAkiModLoader");
+        this.table = container.resolve<DatabaseServer>("DatabaseServer").getTables();
+        this.hideoutProductions = this.table.hideout?.production;
+    }
 
     public preAkiLoad(container: DependencyContainer): void
     {
@@ -112,12 +129,32 @@ class Mod implements IPreAkiLoadMod
             {frequency: "Always"});
         }
 
-        this.hookRoutes();
-    }
+        if (config.craftItems.enabled)
+        {
+            container.afterResolution("HideoutCallbacks", (_t, result: HideoutCallbacks) =>
+            {
+                const oldTakeProduction = result.takeProduction.bind(result);
+                result.takeProduction = (pmcData: IPmcData, body: IHideoutTakeProductionRequestData, sessionID: string) =>
+                {
+                    return this.takeProduction(pmcData, body, sessionID, oldTakeProduction);
+                }
+            },
+            {frequency: "Always"});
 
-    public postAkiLoad(container: DependencyContainer): void
-    {
-        this.modLoader = container.resolve<PreAkiModLoader>("PreAkiModLoader");
+            container.afterResolution("InventoryHelper", (_t, result: InventoryHelper) =>
+            {
+                const oldAddItem = result.addItem.bind(result);
+                result.addItem = (pmcData: IPmcData, request: IAddItemRequestData, output: IItemEventRouterResponse, sessionID: string, callback: {
+                    (): void;
+                }, foundInRaid?: boolean, addUpd?: any) =>
+                {
+                    return this.addItem(pmcData, request, output, sessionID, callback, foundInRaid, addUpd, oldAddItem);
+                }
+            },
+            {frequency: "Always"});
+        }
+
+        this.hookRoutes();
     }
 
     private hookRoutes(): void
@@ -256,6 +293,36 @@ class Mod implements IPreAkiLoadMod
         return assort;
     }
 
+    private takeProduction(pmcData: IPmcData, body: IHideoutTakeProductionRequestData, sessionID: string, oldTakeProduction: (pmcData: IPmcData, body: IHideoutTakeProductionRequestData, sessionID: string) => IItemEventRouterResponse): IItemEventRouterResponse
+    {
+        const hideoutProduction = this.hideoutProductions.find((production) => production._id === body.recipeId);
+
+        if (hideoutProduction)
+        {
+            this.unlockById(hideoutProduction.endProduct, sessionID);
+        }
+        return oldTakeProduction(pmcData, body, sessionID);
+    }
+
+    private addItem(pmcData: IPmcData, request: IAddItemRequestData, output: IItemEventRouterResponse, sessionID: string, callback: {
+        (): void;
+    }, foundInRaid?: boolean, addUpd?: any, oldAddItem?: (pmcData: IPmcData, request: IAddItemRequestData, output: IItemEventRouterResponse, sessionID: string, callback: {
+        (): void;
+    }, foundInRaid?: boolean, addUpd?: any) => IItemEventRouterResponse): IItemEventRouterResponse
+    {
+        const data = oldAddItem(pmcData, request, output, sessionID, callback, foundInRaid, addUpd);
+
+        const itemChanges = output.profileChanges[sessionID].items as ItemChanges;
+        const items = itemChanges.new;
+
+        if (items) {
+            for (const item of items) {
+                this.unlockById(item._tpl, sessionID);
+            }
+        }
+        return data;
+    }
+
     private acceptQuest(pmcData: IPmcData, body: IAcceptQuestRequestData, sessionID: string, oldAcceptQuest: (pmcData: IPmcData, body: IAcceptQuestRequestData, sessionID: string) => IItemEventRouterResponse): IItemEventRouterResponse
     {
         this.unlockQuestsItems(pmcData, body.qid, sessionID, QuestStatus.Started);
@@ -373,19 +440,28 @@ class Mod implements IPreAkiLoadMod
         const logger = Mod.container.resolve<ILogger>("WinstonLogger");
 
         const profile = this.getProfile(sessionID);
+        const origCount = profile.tradeDeficitItems.length;
+        for (const item of items)
+        {
+            this.unlockById(item._tpl, sessionID);
+        }
+        logger.info(`[tradeDeficitItems] Unlocked ${profile.tradeDeficitItems.length - origCount} items for player ${profile.info?.username}`)
+    }
+
+    private unlockById(itemId: string, sessionID: string): void
+    {
+        const logger = Mod.container.resolve<ILogger>("WinstonLogger");
+
+        const profile = this.getProfile(sessionID);
         if (!profile.tradeDeficitItems)
         {
             profile.tradeDeficitItems = [];
         }
 
-        const origCount = profile.tradeDeficitItems.length;
-        for (const item of items)
-        {
-            if (!item._tpl || profile.tradeDeficitItems.includes(item._tpl)) continue;
-            profile.tradeDeficitItems.push(item._tpl);
-            logger.info(`[tradeDeficitItems] Unlocked ${item._tpl} for player ${profile.info?.username}`)
-        }
-        logger.info(`[tradeDeficitItems] Unlocked ${profile.tradeDeficitItems.length - origCount} items for player ${profile.info?.username}`)
+        logger.info(`[tradeDeficitItems] Start unlocking ${itemId} for player ${profile.info?.username}`)
+        if (!itemId || profile.tradeDeficitItems.includes(itemId)) return;
+        profile.tradeDeficitItems.push(itemId);
+        logger.info(`[tradeDeficitItems] Unlocked ${itemId} for player ${profile.info?.username}`)
     }
 }
 
